8f017d1af78e3316a313b787a1ce59e2c6c62a72
Rofida_2000 <mohamedrofida7@gmail.com>
2024-03-13 21:46:03 +0200

    pages of sign

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { SigninFormComponent } from './Component/signin-form/signin-form.component';
+import { SignUpFormComponent } from './Component/sign-up-form/sign-up-form.component';
+import { SignImgComponent } from './Component/sign-img/sign-img.component';
+import { ForgetPasswordComponent } from './Component/forget-password/forget-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninFormComponent,
+    SignUpFormComponent,
+    SignImgComponent,
+    ForgetPasswordComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
