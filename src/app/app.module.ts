1dedbfcbbc9d053f37ae5738c0da1a9ca4d02d7d
Mohamed <my1379@fayoum.edu.eg>
2024-03-14 11:54:41 +0200

    Add Slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './Component/core/main-page/main-page.component';
+import { ReviewsComponent } from './Component/core/reviews/reviews.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainPageComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MainPageComponent, ReviewsComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
