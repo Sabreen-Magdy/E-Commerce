883b5d016df295f9a38328d428df8170a078df2c
Mohamed <my1379@fayoum.edu.eg>
2024-03-21 01:39:29 +0200

    sallerAdd gitignore to Back-Branch

---
+++ Contract/Saller/SallerDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contract.Saller
+{
+    public record SallertDto
+    {
+        public int Id { get; set; }
+        public int NId { get; set; }
+
+        public string Name { get; set; } = null!;
+        public string Password { get; set; }
+        public string Phono { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        // public List<ItemDto> items { get; set; } = null!;
+    }
+}
