8c747b0ac4ef398adbdc347477bfb1a0f212936d
Mohamed <my1379@fayoum.edu.eg>
2024-03-21 01:30:06 +0200

    Organize Master I

--- Back-End/Contract/Cart/CartDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using Contract.OrderItem;
-
-namespace Contract.Cart;
-
-public record CartDto
-{
-    public double TotalPrice { get; set; }
-    public List<ItemDto> items { get; set; } = null!;
-}
--- Back-End/Domain/Exceptions/PropertyException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Domain.Exceptions;
-
-public class PropertyException : Exception
-{
-    public PropertyException(string propertyName) :
-        base($"{propertyName} is not a property")
-    { }
-    public override string Message => "Property is invalid";
-}
