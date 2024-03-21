c0b07f78235b24a60e1a605df389d0d772338572
Mohamed <my1379@fayoum.edu.eg>
2024-03-21 21:18:32 +0200

    Fix Some Errors II

--- Domain/Entities/Saller.cs
+++ Domain/Entities/Saller.cs
@@ -1,6 +1,11 @@
 ﻿namespace Domain.Entities;
 
-public class Saller : BaseEntity
+public record Saller 
 {
-
+    public int Id { get; set; }
+    public int NId { get; set; }
+    public string Name { get; set; } = null!;
+    public string Password { get; set; }
+    public string Phono { get; set; } = null!;
+    public string Email { get; set; } = null!;
 }
