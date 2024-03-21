16375fe2ae52a42bc104eb10d2afac8bf25d783f
Mohamed <my1379@fayoum.edu.eg>
2024-03-21 19:26:51 +0200

    Comoplete Pull

--- Domain/Entities/saller.cs
+++ Domain/Entities/saller.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Domain.Entities;
 
-namespace Domain.Entities
+public record Saller 
 {
-    public record saller 
-    {
-        public int Id { get; set; }
-        public int NId { get; set; }
-        public string Name { get; set; } = null!;
-        public string Password { get; set; }
-        public string Phono { get; set; } = null!;
-        public string Email { get; set; } = null!;
-    }
+    public int Id { get; set; }
+    public int NId { get; set; }
+    public string Name { get; set; } = null!;
+    public string Password { get; set; }
+    public string Phono { get; set; } = null!;
+    public string Email { get; set; } = null!;
 }
