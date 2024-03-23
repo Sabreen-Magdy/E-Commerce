8c31be9093286257dd0a1f641cada1f07abfe420
Mohamed Yassere <my1379@fayoum.edu.eg>
2024-03-23 12:06:44 +0200

    Complete Merge

--- Domain/Entities/saller.cs
+++ Domain/Entities/saller.cs
@@ -6,7 +6,7 @@ public record Saller
     public int NId { get; set; }
     public string Name { get; set; } = null!;
     public string Password { get; set; } = null!;
-    public string Phono { get; set; } = null!;
+    public string Phone { get; set; } = null!;
     public string Email { get; set; } = null!;
 
     public List<Product> Products { get; set; } = null!;
