d1a25b98dc779179806e79c017290d6cd7f983b3
Sama-Mahmoud <samafawzy373@gmail.com>
2024-03-25 12:48:35 +0200

    comment cart

--- Presentation/Controllers/CartController.cs
+++ Presentation/Controllers/CartController.cs
@@ -1,69 +1,69 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿//using Microsoft.AspNetCore.Mvc;
 
-using Domain.Entities;
-using Services.Abstraction.DataServices;
-using Contract;
-using Domain.Enums;
-namespace Presentation.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class CartController : ControllerBase
-    {
-        private readonly IAdminService _adminService;
+//using Domain.Entities;
+//using Services.Abstraction.DataServices;
+//using Contract;
+//using Domain.Enums;
+//namespace Presentation.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class CartController : ControllerBase
+//    {
+//        private readonly IAdminService _adminService;
 
-        public CartController(IAdminService adminService)
-        {
-            _adminService = adminService;
-        }
+//        public CartController(IAdminService adminService)
+//        {
+//            _adminService = adminService;
+//        }
 
-        [HttpGet("{id}")]
-        public IActionResult GetById(int id)
-        {
-            var cart = _adminService.CartService.Get(id);
-            if (cart == null)
-            {
-                return NotFound();
-            }
-            return Ok(cart);
-        }
-        [HttpGet("customer/{customerId}")]
-        public IActionResult GetByCustomerId(int customerId)
-        {
-            var cart = _adminService.CartService.GetByCustomerId(customerId);
-            if (cart == null)
-            {
-                return NotFound();
-            }
-            return Ok(cart);
-        }
-        [HttpPost]
-        public IActionResult AddItemToCart(ItemDto itemDto, int cartId)
-        {
-            _adminService.CartService.AddItemToCart(itemDto, cartId);
-            return NoContent();
-        }
+//        [HttpGet("{id}")]
+//        public IActionResult GetById(int id)
+//        {
+//            var cart = _adminService.CartService.Get(id);
+//            if (cart == null)
+//            {
+//                return NotFound();
+//            }
+//            return Ok(cart);
+//        }
+//        [HttpGet("customer/{customerId}")]
+//        public IActionResult GetByCustomerId(int customerId)
+//        {
+//            var cart = _adminService.CartService.GetByCustomerId(customerId);
+//            if (cart == null)
+//            {
+//                return NotFound();
+//            }
+//            return Ok(cart);
+//        }
+//        [HttpPost]
+//        public IActionResult AddItemToCart(ItemDto itemDto, int cartId)
+//        {
+//            _adminService.CartService.AddItemToCart(itemDto, cartId);
+//            return NoContent();
+//        }
 
-        [HttpPut]
-        public IActionResult UpdateCart(Cart cart)
-        {
-            _adminService.CartService.Update(cart.CustomerId);
-            return NoContent();
-        }
+//        [HttpPut]
+//        public IActionResult UpdateCart(Cart cart)
+//        {
+//            _adminService.CartService.Update(cart.CustomerId);
+//            return NoContent();
+//        }
 
-        [HttpPut]
-        public IActionResult UpdateCartItem(int id, Dictionary<Properties, int> newValues)
-        {
-            _adminService.CartService.UpdateItem(id,newValues);
-            return NoContent();
-        }
+//        [HttpPut]
+//        public IActionResult UpdateCartItem(int id, Dictionary<Properties, int> newValues)
+//        {
+//            _adminService.CartService.UpdateItem(id,newValues);
+//            return NoContent();
+//        }
 
-        [HttpDelete("{id}")]
-        public IActionResult DeleteCartItem(int id)
-        {
-            _adminService.CartService.DeleteItem(id);
-            return NoContent();
-        }
+//        [HttpDelete("{id}")]
+//        public IActionResult DeleteCartItem(int id)
+//        {
+//            _adminService.CartService.DeleteItem(id);
+//            return NoContent();
+//        }
 
-    }
-}
+//    }
+//}
--- Presentation/Controllers/FavouriteController.cs
+++ Presentation/Controllers/FavouriteController.cs
@@ -1,50 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Domain.Repositories;
-using Domain.Entities;
-using Services.Abstraction.DataServices;
-using Contract.Favorite;
-namespace Presentation.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class FavouriteController : ControllerBase
-    {
-        private readonly IAdminService _adminService;
+﻿//using Microsoft.AspNetCore.Mvc;
+//using Domain.Repositories;
+//using Domain.Entities;
+//using Services.Abstraction.DataServices;
+//using Contract.Favorite;
+//namespace Presentation.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class FavouriteController : ControllerBase
+//    {
+//        private readonly IAdminService _adminService;
 
-        public FavouriteController(IAdminService adminService) =>
-            _adminService = adminService;
+//        public FavouriteController(IAdminService adminService) =>
+//            _adminService = adminService;
 
-        [HttpGet("{Id}")]
-        public IActionResult GetByProductId(int Id)
-        {
-            var fav = _adminService.FavouriteService.GetById(Id);
-            if (fav == null)
-            {
-                return NotFound();
-            }
-            return Ok(fav);
-        }
-        [HttpGet("customer/{customerId}")]
-        public IActionResult GetByCustomerId(int customerId)
-        {
-            var fav = _adminService.FavouriteService.GetAllByCustomerId(customerId);
-            if (fav == null)
-            {
-                return NotFound();
-            }
-            return Ok(fav);
-        }
-        [HttpPost]
-        public IActionResult AddFavorite(FavoriteDto favourite)
-        {
-            _adminService.FavouriteService.AddFavorite(favourite);
-            return Ok();
-        }
-        [HttpDelete("{id}")]
-        public IActionResult DeleteFavorite(int id)
-        {
-            _adminService.FavouriteService.DeleteItem(id);
-            return Ok();
-        }
-    }
-}
+//        [HttpGet("{Id}")]
+//        public IActionResult GetByProductId(int Id)
+//        {
+//            var fav = _adminService.FavouriteService.GetById(Id);
+//            if (fav == null)
+//            {
+//                return NotFound();
+//            }
+//            return Ok(fav);
+//        }
+//        [HttpGet("customer/{customerId}")]
+//        public IActionResult GetByCustomerId(int customerId)
+//        {
+//            var fav = _adminService.FavouriteService.GetAllByCustomerId(customerId);
+//            if (fav == null)
+//            {
+//                return NotFound();
+//            }
+//            return Ok(fav);
+//        }
+//        [HttpPost]
+//        public IActionResult AddFavorite(FavoriteDto favourite)
+//        {
+//            _adminService.FavouriteService.AddFavorite(favourite);
+//            return Ok();
+//        }
+//        [HttpDelete("{id}")]
+//        public IActionResult DeleteFavorite(int id)
+//        {
+//            _adminService.FavouriteService.DeleteItem(id);
+//            return Ok();
+//        }
+//    }
+//}
