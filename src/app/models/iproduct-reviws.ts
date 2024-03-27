568b026debb7eb379c6ddfa2deb902927f03b03c
Sama-Mahmoud <samafawzy373@gmail.com>
2024-03-27 18:08:05 +0200

    app-review

---
+++ src/app/models/iproduct-reviws.ts
@@ -0,0 +1,10 @@
+export interface IproductReviws {
+
+    productId: number
+    productName: string
+    customerId: number
+    customerName: string
+    review: string
+    rate: number
+    date: string
+}
