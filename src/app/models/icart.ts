7c1ab86cabdc689576ef5854f48fc04ce5894a8a
shroukmohamed 30203242200708 <shroukmohamed896_sd@fcis.bsu.edu.eg>
2024-03-27 21:41:39 +0200

    rok

--- src/app/models/icart.ts
+++ src/app/models/icart.ts
@@ -2,4 +2,15 @@ export interface Icart {
   Id:number;
 TotalPrice :number
  TotalQuantity :Number
+ ITem:Item[]
+}
+export interface Item {
+  
+  id:Number;
+  img:string;
+  Name:string;
+  Description:String;
+  Quantitiy:number;
+  price:number
+
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Icart } from '../models/icart';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  baseURL :string ="http://localhost:5058/api/Cart/";
-  constructor() { }
+  baseURL :string ="http://localhost:5058/api/Cart";
+  constructor( private http: HttpClient) { }
+  getAllCart() : Observable<Icart[]>{
+
+    return this.http.get<Icart[]>(`${this.baseURL}GetAll`)
+  } 
+  getCartById(id: number): Observable<Icart> {
+    return this.http.get<Icart>(`${this.baseURL}/GetById/${id}`);
+  }
+
+  addCart(cart: Icart): Observable<Icart> {
+    return this.http.post<Icart>(`${this.baseURL}/Add`, cart);
+  }
+
+  updateCart(cart: Icart): Observable<Icart> {
+    return this.http.put<Icart>(`${this.baseURL}/Update`, cart);
+  }
+
+  deleteCart(id: number): Observable<any> {
+    return this.http.delete(`${this.baseURL}/Delete/${id}`);
+  }
 }
