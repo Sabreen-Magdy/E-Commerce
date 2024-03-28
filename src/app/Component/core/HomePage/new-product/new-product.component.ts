a3bb451dab6bf682be811fa2ea137e53f438f6b6
Sabreen magdy <sabreenmagdy973@gmail.com>
2024-03-28 21:43:04 +0200

    favorite&admin

--- src/app/Component/core/HomePage/new-product/new-product.component.ts
+++ src/app/Component/core/HomePage/new-product/new-product.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/auth.service';
+import { Ifav } from 'src/app/models/Ifav';
 import { IproductShow } from 'src/app/models/i-product-variant';
+import { FavoriteService } from 'src/app/services/favorite.service';
 import { ProductFormService } from 'src/app/services/product-form.service';
 
 @Component({
@@ -11,15 +14,15 @@ import { ProductFormService } from 'src/app/services/product-form.service';
 export class NewProductComponent implements OnInit {
 
   allproductList :IproductShow[] =[];
-
-  constructor( private prodServ : ProductFormService ){}
+  customerId:number=0;
+  constructor( private prodServ : ProductFormService,private _authService:AuthService,private _favService:FavoriteService){}
 
   ngOnInit(): void {
+    this.customerId=this._authService.id;
     this.getallproduct();
   }
 
   AllProductSub : Subscription | undefined;
-
   getallproduct(){
     console.log("doooone");
     this.AllProductSub = this.prodServ.getAllProduct().subscribe({
@@ -34,4 +37,16 @@ export class NewProductComponent implements OnInit {
   }
 
 
+  pushItemToFavCart(id:number,image:string,name:string,price:number){
+    console.log(image);
+    let favoriteItem : Ifav = {
+      CustomerId:this.customerId,
+      ProductId :id,
+      Image :image,
+      Name:name,
+      Description :"ffff",
+      Price :price
+  }
+    this._favService.addToFavorite(favoriteItem);
+  }
 }
--- src/app/services/favorite.service.ts
+++ src/app/services/favorite.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Ifav } from '../models/Ifav';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FavoriteService {
-  baseURL :string ="http://localhost:5058/api//";
-  constructor() { }
+  baseURL :string ="http://localhost:5058/api/Favourite/";
+  secondBaseUrl:string="http://localhost:5058/api/Customer/";
+  constructor(private _HttpClient:HttpClient,private _Router:Router) {
+    if(localStorage.getItem('loginToken') != null){
+      // this.saveUserData();
+    }
+  }
+
+addToFavorite(favItem:Ifav):Observable<any>
+{
+  return this._HttpClient.post(`${this.baseURL}AddFavorite/`,favItem);
+}
+getFavoriteByCustomerId(customerId:number):Observable<any>
+{
+  return this._HttpClient.get(`http://localhost:5058/api/Customer/GetFavourites?id=${customerId}`);
+}
+
 }
