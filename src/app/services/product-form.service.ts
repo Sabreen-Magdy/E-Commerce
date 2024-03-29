878ae6e6509b971b8910903bf3245580e5b75961
Sama-Mahmoud <samafawzy373@gmail.com>
2024-03-29 02:54:17 +0200

    prod

--- src/app/services/product-form.service.ts
+++ src/app/services/product-form.service.ts
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
-import { Observable } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-=======
 
->>>>>>> 880e513baff1cd6923d2a77d915fc197972b8fd7
+// import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+// import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { IProductAddForm, IproductShow } from '../models/i-product-variant';
@@ -38,36 +35,36 @@ export class ProductFormService {
       // );
   }
 
-  // fetchAndCacheProducts(): Observable<IproductShow[]> {
-  //   console.log("fetch");
-  //   if (this.productsCache.length == 0 ) {
-  //     console.log("fetch form service");
-  //     return this.http.get<IproductShow[]>(`${this.baseURL2}GetAll`).pipe(
-  //       tap(products => this.productsCache = products) // Cache the products
-  //     );
-  //   }  
-  //   else {
+  fetchAndCacheProducts(): Observable<IproductShow[]> {
+    console.log("fetch");
+    if (this.productsCache.length == 0 ) {
+      console.log("fetch form service");
+      return this.http.get<IproductShow[]>(`${this.baseURL2}GetAll`).pipe(
+        tap(products => this.productsCache = products) // Cache the products
+      );
+    }  
+    else {
 
-  //     console.log("osjsk");
-  //     // If products are cached, return the cached products as an Observable
-  //     return of(this.productsCache);
+      console.log("osjsk");
+      // If products are cached, return the cached products as an Observable
+      return of(this.productsCache);
+    }
+  }
+
+
+  get length (){
+    return 0 
+  }
+  getAllProduct2(): Observable<IproductShow[]> {
+    console.log("getAllproduct2");
+    return this.fetchAndCacheProducts();
+  }
+
+  // getState(){
+  //   if (this.productsCache.length == 0) {
+
   //   }
   // }
-
-
-  // get length (){
-  //   return 0 
-  // }
-  // getAllProduct2(): Observable<IproductShow[]> {
-  //   console.log("getAllproduct2");
-  //   return this.fetchAndCacheProducts();
-  // }
-
-  // // getState(){
-  // //   if (this.productsCache.length == 0) {
-
-  // //   }
-  // // }
 
 
   getAllProduct () : Observable <IproductShow[]> {
