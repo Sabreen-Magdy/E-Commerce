c2e191128622ce8913c781c45db48afb5b7a48a9
Sama-Mahmoud <samafawzy373@gmail.com>
2024-03-30 21:18:15 +0200

    edit profile done 011 validation in sign up and edit profile

--- src/app/Component/core/ProfilePage/editprofile/editprofile.component.ts
+++ src/app/Component/core/ProfilePage/editprofile/editprofile.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth.service';
@@ -8,44 +8,72 @@ import { AuthService } from 'src/app/auth.service';
   templateUrl: './editprofile.component.html',
   styleUrls: ['./editprofile.component.css']
 })
-export class EditprofileComponent {
-  editProfileForm : FormGroup;
+export class EditprofileComponent implements OnInit {
+
+  editProfileForm : FormGroup=new FormGroup({});
   showerror : boolean = false;
   constructor(private _AuthService:AuthService,private _Router:Router){
-    this.editProfileForm = new FormGroup({
-      name: new FormControl(
-        "",
-        [
-          Validators.required,
-          Validators.minLength(5),
-          Validators.pattern('[\u0600-\u06FF ,]+')
-        ]
-      ),
-      email: new FormControl(
-        "",
-        [
-          Validators.required,
-          Validators.pattern(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)
-        ]
-      ),
-      phone : new FormControl(
-        "",
-        [
-          Validators.pattern(/^(010|012|015)[0-9]{8}$/)
-        ]
-      ),
-      password : new FormControl (
-        "",
-        [
-          Validators.required,
-          Validators.minLength(10),
-
-        ]
-      ),
-    }
-    );
+    this.MakeFormValidation()
   }
+  ngOnInit(): void {
+    this.getDataOfUser()
+  }
+  nameValue:string =""
+  EmailValue:string =""
+  PhoneValue:string =""
+  PassValue:string =""
 
+  getDataOfUser(){
+    this._AuthService.userData.subscribe({
+      next:()=>{
+        if(this._AuthService.userData.getValue()!=null){
+          this._AuthService.getDataOfUser().subscribe(
+            { next:(response)=>{
+                this.nameValue= response.name;
+                this.EmailValue=response.email;
+                this.PhoneValue= response.phone;
+                this.PassValue=response.password;
+                this.MakeFormValidation()
+          }
+        });
+        }
+      }
+    });
+  }
+MakeFormValidation(){
+  this.editProfileForm = new FormGroup({
+    name: new FormControl(
+      this.nameValue,
+      [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.pattern('[\u0600-\u06FF ,]+')
+      ]
+    ),
+    email: new FormControl(
+      this.EmailValue,
+      [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z0-9_-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,4}$/)
+      ]
+    ),
+    phone : new FormControl(
+      this.PhoneValue,
+      [
+        Validators.pattern(/^(010|012|015|011)[0-9]{8}$/)
+      ]
+    ),
+    password : new FormControl (
+      this.PassValue,
+      [
+        Validators.required,
+        Validators.minLength(5),
+
+      ]
+    ),
+  }
+  );
+}
    enter(e:Event){
     console.log(this.editProfileForm.valid);
    }
@@ -70,7 +98,7 @@ export class EditprofileComponent {
       this._AuthService.updateUser(this.editProfileForm.value).subscribe({
         next:(response)=>{
           this._Router.navigate(['/profile/profiledetails']);
-
+          // location.reload();
         },
         error : (e) => {
           console.log(e);
